7349fea06f5f7bc60729e76c0835eaeed312c974
yu0722 <2973006511@qq.com>
2024-07-04 17:55:35 +0800

    feat:init

---
+++ src/main/java/org/example/hypersdkconnector/HyperSdkConnectorApplication.java
@@ -0,0 +1,13 @@
+package org.example.hypersdkconnector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyperSdkConnectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HyperSdkConnectorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/hypersdkconnector/HyperSdkConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hypersdkconnector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyperSdkConnectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
